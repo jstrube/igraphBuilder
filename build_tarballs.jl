41748b52f3f9c64a4b9aa01999e4d276e1eff855
Jan Strube <jan.strube@gmail.com>
2019-03-24 11:00:36 -0700

    disabling architecture that's broken on travis

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -27,7 +27,7 @@ make install
 platforms = [
     Linux(:i686, libc=:glibc),
     Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
+#    Linux(:aarch64, libc=:glibc),
     Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
     Linux(:powerpc64le, libc=:glibc),
     MacOS(:x86_64)
